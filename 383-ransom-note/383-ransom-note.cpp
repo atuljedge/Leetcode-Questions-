fed285d04b64215246a4704c8c03a8ace3a08aac
atuljedge <95165885+atuljedge@users.noreply.github.com>
2022-08-26 19:14:37 +0530

    Time: 37 ms (31.12%), Space: 8.8 MB (39.49%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>m;
+        for(int i=0;i<magazine.size();i++)
+            m[magazine[i]]++;
+        for(int i=0;i<ransomNote.size();i++)
+            if(--m[ransomNote[i]]<0)
+                return false;
+        return true;
+    }
+};
